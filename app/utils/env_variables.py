f3f41a18e856a56a3a16167a9e141ef5514ba794
himanshu-sagar <sagarhimanshu355@gmail.com>
2022-05-02 15:02:43 +0530

    adding procfile

--- app/utils/env_variables.py
+++ app/utils/env_variables.py
@@ -4,7 +4,7 @@ import base64
 load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 WEATHER_API_KEY = os.environ.get('WEATHER_API_KEY')
 ####### Constants #######
 POSTS_TABLE = 'posts'
